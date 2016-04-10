08aee29cf66f113f2bd19bfecb87ef9395eb3102
Jens <jens_johansen1@hotmail.com>
2016-04-10 18:16:24 +0200

    plots update

--- plots/plot.cmd
+++ plots/plot.cmd
@@ -1,5 +1,7 @@
-filename = "pid12"
+infilename = "pid.csv"
+outfilename = "pid_filter_"
+outfilenumber = 14
 set datafile separator ","
 set terminal png size 1920,1024 enhanced font "Helvetica,20"
-set output filename.".png"
-plot filename.".csv" using 1:2 with lines, filename.".csv" using 1:3 with lines, filename.".csv" using 1:4 with lines
+set output outfilename.outfilenumber.".png"
+plot infilename using 1:2 with lines
