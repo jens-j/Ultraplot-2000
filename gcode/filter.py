edaa5fbb36986a1d6b106260c6035c4a253023f0
Jens <jens_johansen1@hotmail.com>
2016-05-09 20:05:05 +0200

    added a python script to parse the gcode files removing duplicate paths

---
+++ gcode/filter.py
@@ -0,0 +1,104 @@
+
+class Filter:
+
+    def __init__(self):
+        self.fin = open("octopus_offset.ngc", "r")
+        self.fout = open("test.ngc", "w")
+        self.count = 0 
+        self.seen = []
+
+    def close(self):
+        self.fin.close()
+        self.fout.close()
+
+    def readLine(self):
+        pass 
+        while(True):
+
+            self.count += 1
+
+            s = self.fin.readline()
+
+            # try:
+            #     s = self.fin.readline()
+            # except:
+            #     print "unable to read line"
+            #     return None
+            #     quit()
+
+            if not s:
+                return None
+            if len(s) is 0:
+                continue
+            elif s[0] is 'G' and s[1] is '0':
+                return s
+
+
+
+
+    def writeLine(self, s):
+        self.fout.write(s)
+
+
+    def readPath(self):
+        ops = []
+
+        s = self.readLine()
+        if s is None:
+            return None
+
+        if not ("G00 Z5.000000" in s):
+            print "invalid start of path\n"
+            print s
+            quit()
+
+        ops.append(s)
+
+        s = self.readLine()
+        if s is None:
+            return None
+
+        while not ("G00 Z5.000000" in s):
+            #print s
+            ops.append(s)
+            s = self.readLine()
+            if s is None:
+                return None
+
+        #ops.append(s)
+        return ops
+
+
+    def filterPaths(self):
+
+        p = self.readPath()
+
+        while( not ( p is None) ):
+
+            if not (p[3] in self.seen):
+                print p[3]
+                self.seen.append(p[3])
+                for line in p:
+                    self.writeLine(line)
+
+            p = self.readPath()
+
+
+
+if __name__ == "__main__":
+
+    filter = Filter()
+
+    # while(True):
+    #     s = filter.readLine()
+    #     if s is None:
+    #         break
+    #     else:
+    #         filter.writeLine(s)
+
+    #print filter.readPath()
+
+    filter.filterPaths()
+
+    filter.close()
+    quit()
